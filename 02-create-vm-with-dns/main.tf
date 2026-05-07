efef3e83d68c42950f6fa5c25c1d2b1e2937eea5
reachmohancareers88 <reachmohancareers@gmail.com>
2026-05-07 16:54:57 +0530

    Update main.tf

--- 02-create-vm-with-dns/main.tf
+++ 02-create-vm-with-dns/main.tf
@@ -3,6 +3,16 @@ provider "azurerm" {
   features {}
 }
 
+# Create Public IP
+resource "azurerm_public_ip" "main" {
+  name                = "test-public-ip"
+  location            = "East US"
+  resource_group_name = "denmark-east-rg"
+
+  allocation_method = "Static"
+}
+
+# Create Network Interface
 resource "azurerm_network_interface" "main" {
   name                = "test-nic"
   location            = "East US"
@@ -10,25 +20,29 @@ resource "azurerm_network_interface" "main" {
 
   ip_configuration {
     name                          = "testconfiguration1"
-    subnet_id                     = "/subscriptions/cde5241e-289a-449b-b2b7-4efcf2d5c83c/resourceGroups/denmark-east-rg/providers/Microsoft.Network/virtualNetworks/Terraform-vnet/subnets/default"
+
+    subnet_id = "/subscriptions/cde5241e-289a-449b-b2b7-4efcf2d5c83c/resourceGroups/denmark-east-rg/providers/Microsoft.Network/virtualNetworks/Terraform-vnet/subnets/default"
+
     private_ip_address_allocation = "Dynamic"
+
+    public_ip_address_id = azurerm_public_ip.main.id
   }
 }
 
+# Create Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "main" {
-  name                  = "test-vm"
-  location              = "East US"
-  resource_group_name   = "denmark-east-rg"
-  network_interface_ids = [azurerm_network_interface.main.id]
+
+  name                = "test-vm"
+  location            = "East US"
+  resource_group_name = "denmark-east-rg"
+
+  network_interface_ids = [
+    azurerm_network_interface.main.id
+  ]
 
   size = "Standard_D2s_v3"
 
   source_image_id = "/subscriptions/cde5241e-289a-449b-b2b7-4efcf2d5c83c/resourceGroups/denmark-east-rg/providers/Microsoft.Compute/galleries/image/images/Imagedefinition/versions/1.0.0"
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
 
   admin_username = "Devops"
   admin_password = "DevOps@123456"
@@ -37,4 +51,9 @@ resource "azurerm_linux_virtual_machine" "main" {
 
   secure_boot_enabled = true
   vtpm_enabled        = true
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 }
