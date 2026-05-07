30393d7d4914f6958c2811496ca11a426d0c278e
Learnshell <reachmohancareers@gmail.com>
2026-05-07 16:15:03 +0530

    Commit VM DNS

--- 02-create-vm-with-dns/main.tf
+++ 02-create-vm-with-dns/main.tf
@@ -5,37 +5,36 @@ provider "azurerm" {
 
 resource "azurerm_network_interface" "main" {
   name                = "test-nic"
-  location            = "Denmark East"
+  location            = "East US"
   resource_group_name = "denmark-east-rg"
 
   ip_configuration {
     name                          = "testconfiguration1"
-    subnet_id                     = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Network/virtualNetworks/rhel10-vmVNET/subnets/rhel10-vmSubnet"
+    subnet_id                     = "/subscriptions/cde5241e-289a-449b-b2b7-4efcf2d5c83c/resourceGroups/denmark-east-rg/providers/Microsoft.Network/virtualNetworks/Terraform-vnet/subnets/default"
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-
 resource "azurerm_linux_virtual_machine" "main" {
   name                  = "test-vm"
-  location              = "Denmark East"
+  location              = "East US"
   resource_group_name   = "denmark-east-rg"
   network_interface_ids = [azurerm_network_interface.main.id]
-  size               = "Standard_D2s_v3"
 
-  source_image_id = "/subscriptions/3f2e42e1-ca06-4a99-8c56-be8d8ba306db/resourceGroups/denmark-east-rg/providers/Microsoft.Compute/galleries/rhel10/images/1.0.0/versions/1.0.0"
+  size = "Standard_D2s_v3"
+
+  source_image_id = "/subscriptions/cde5241e-289a-449b-b2b7-4efcf2d5c83c/resourceGroups/denmark-east-rg/providers/Microsoft.Compute/galleries/image/images/Imagedefinition/versions/1.0.0"
 
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
+  admin_username = "Devops"
   admin_password = "DevOps@123456"
-  admin_username = "devops"
 
   disable_password_authentication = false
 
   secure_boot_enabled = true
   vtpm_enabled        = true
-
 }
